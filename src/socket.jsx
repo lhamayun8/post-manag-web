b7835c4c1dcf0b3b343b3d2b89e61968f371f6fa
Laiba <hamayunlaiba23@gmail.com>
2026-08-18 13:28:23 +0500

    fixes in CORS

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,4 +1,10 @@
 import { io } from "socket.io-client";
-const socket = io(import.meta.env.VITE_API_URL);
+
+const socket = io(
+  "https://exquisite-empathy-production-97e7.up.railway.app",
+  {
+    transports: ["websocket", "polling"],
+  }
+);
 
 export default socket;

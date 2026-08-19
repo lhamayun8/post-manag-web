4701bd25c630ff588ad3dcb690cf4102f7d294e8
Laiba <hamayunlaiba23@gmail.com>
2026-08-19 14:02:56 +0500

    testing cron job

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -4,5 +4,5 @@ from zoneinfo import ZoneInfo
 scheduler=AsyncIOScheduler(timezone=ZoneInfo("Asia/Karachi"))
 
 def startscheduler():
-    scheduler.add_job(sendrecommendations,trigger="cron",hour=11,minute=25,id="dailypostrecommendation",replace_existing=True,misfire_grace_time=300)
+    scheduler.add_job(sendrecommendations,trigger="cron",hour=2,minute=5,id="dailypostrecommendation",replace_existing=True,misfire_grace_time=300)
     scheduler.start()
